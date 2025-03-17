787c25ee463971ffc4d4b4396947a5316f1a160b
dramak1ng <kensi11qw@gmail.com>
2025-03-17 18:31:14 +0300

    JMP-34: Rename template-service to order-service and update related configurations.

--- src/main/java/com/jmp/orderservice/OrderMicroserviceApplication.java
+++ src/main/java/com/jmp/orderservice/OrderMicroserviceApplication.java
@@ -1,11 +1,11 @@
-package com.jmp.template;
+package com.jmp.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateMicroserviceApplication {
+public class OrderMicroserviceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TemplateMicroserviceApplication.class, args);
+        SpringApplication.run(OrderMicroserviceApplication.class, args);
     }
 }
